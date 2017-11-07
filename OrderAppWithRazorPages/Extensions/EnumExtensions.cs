7089db4416bc76032b79b34fa949fe9986afd9e8
hishamco <hishamco_2007@yahoo.com>
2017-11-07 23:24:30 +0300

    Add OrderStatus

---
+++ OrderAppWithRazorPages/Extensions/EnumExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace OrderApp.Extensions
+{
+    public static class EnumExtensions
+    {
+        public static T GetAttribute<T>(this Enum value) where T : Attribute
+        {
+            var type = value.GetType();
+            var memberInfo = type.GetMember(value.ToString());
+            var attributes = memberInfo[0].GetCustomAttributes(typeof(T), false);
+
+            return (T)attributes[0];
+        }
+    }
+}
---
+++ OrderAppWithRazorPages/Models/OrderStatus.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+
+namespace OrderApp.Models
+{
+    public enum OrderStatus
+    {
+        [Description("Ordered")]
+        Ordered,
+
+        [Description("Filled")]
+        Filled,
+
+        [Description("Shipped")]
+        Shipped,
+
+        [Description("Out for Delivery")]
+        OutForDelivery,
+
+        [Description("Delivered")]
+        Delivered
+    }
+}
