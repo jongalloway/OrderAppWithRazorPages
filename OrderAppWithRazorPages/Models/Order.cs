ce717fb52355749f7300a415003c6add39caccd3
hishamco <hishamco_2007@yahoo.com>
2017-11-07 21:39:43 +0300

    Add whitespaces

--- OrderAppWithRazorPages/Models/Order.cs
+++ OrderAppWithRazorPages/Models/Order.cs
@@ -5,12 +5,19 @@ namespace OrderApp.Models
     public class Order
     {
         public int OrderId { get; set; }
+
         public DateTime Date { get; set; }
+
         public string Name { get; set; }
+
         public string Address { get; set; }
+
         public string Country { get; set; }
+
         public string Continent { get; set; }
+
         public string Language { get; set; }
+
         public string Phone { get; set; }
     }
 }
--- OrderAppWithRazorPages/Models/Product.cs
+++ OrderAppWithRazorPages/Models/Product.cs
@@ -3,7 +3,9 @@
     public class Product
     {
         public int ProductId { get; set; }
+
         public string Name { get; set; }
+
         public decimal Price { get; set; }
     }
 }
